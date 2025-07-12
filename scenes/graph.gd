918f0fe284a1326d45aa10cc9be38856d598f27a
David Garcia <david.garcia.orozco@intel.com>
2025-07-11 22:01:00 -0400

    Initial graph visualizer

---
+++ scenes/graph.gd
@@ -0,0 +1,45 @@
+extends PanelContainer
+
+var margin
+var margin_min
+var margin_max
+var graph = []
+var graph_max = 20.0
+
+func _draw():
+	margin = max(size.x * 0.05, size.y*0.05)
+	margin_min = Vector2(margin, margin)
+	margin_max = size-margin_min
+	if not graph.is_empty():
+		draw_graph()
+	draw_graph_bounds()
+
+func draw_graph():
+	var points = []
+	var colors = []
+	var i = 0
+	var i_step = (margin_max.x-margin_min.x)/(graph.size()-1)
+	for p in graph:
+		if points.size()>1:
+			points.append(points.back())
+		var alpha = (p/graph_max)
+		var x = margin_min.x+i
+		var y = margin_max.y+(margin_min.y-margin_max.y)*alpha
+		if not points.is_empty():
+			if points.back().y < y:
+				colors.append(Color.RED)
+			else:
+				colors.append(Color.GREEN)
+		points.append(Vector2(x,y))
+		i=i+i_step
+	draw_multiline_colors(points, colors, 1.0, true)
+
+func draw_graph_bounds():
+	draw_line(margin_min, Vector2(margin_min.x, margin_max.y), Color.BLACK, 1.5, true)
+	draw_line(Vector2(margin_min.x, margin_max.y), margin_max, Color.BLACK, 1.5, true)
+
+func _on_timer_timeout():
+	graph.clear()
+	for i in range(100):
+		graph.append(randf()*graph_max)
+	queue_redraw()
